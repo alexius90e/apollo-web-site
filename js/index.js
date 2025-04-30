60c69fba2276d3f2ef2eaeaed45051a58d12e6a3
Aliaksei Karzhou <alexius90e@gmail.com>
2025-04-30 22:21:28 +0300

    feat: added main nav

--- js/index.js
+++ js/index.js
@@ -21,3 +21,39 @@ const designDepartmentSwiper = new Swiper('.design-department .swiper', {
     prevEl: '.swiper-button-prev',
   },
 });
+
+/// main nav
+
+const mainNav = document.querySelector('.main-nav');
+const headerBurgerButton = document.querySelector('.header__burger-button');
+
+if (mainNav && headerBurgerButton) {
+  headerBurgerButton.addEventListener('click', () => {
+    mainNav.classList.add('active');
+  });
+
+  mainNav.addEventListener('click', (event) => {
+    const isLayout = event.target === event.currentTarget;
+    const isCloseButton = event.target.classList.contains('main-nav__body-top-close-button');
+    const isLink = event.target.classList.contains('main-nav__menu-item-link');
+
+    if (isLayout || isCloseButton || isLink) {
+      event.currentTarget.classList.remove('active');
+    }
+  });
+}
+
+//// main-nav__menu-item
+
+const mainNavMenuTogglers = document.querySelectorAll('.main-nav__menu-title');
+
+mainNavMenuTogglers.forEach((toggler) => {
+  toggler.addEventListener('click', (event) => {
+    const togglerParentElement = toggler.parentElement;
+    const isMenuItem = togglerParentElement.classList.contains('main-nav__menu-item');
+
+    if (isMenuItem) {
+      togglerParentElement.classList.toggle('active');
+    }
+  });
+});
